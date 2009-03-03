4c6c9ed405f28bdf74709b590b07e34674d9c312
Alex McHale <alexmchale@gmail.com>
2009-03-03 11:23:46 -0600

    Moved most of the commerce code into its own class.  Added a summary method to show a nice account summary.

--- commercebank.rb
+++ commercebank.rb
@@ -10,10 +10,25 @@ require 'ftools'
 require 'time'
 require 'date'
 require 'json'
+require 'htmlentities'
+
+class Array
+  def binary
+    map {|e| yield(e) ? [e, nil] : [nil, e]}.transpose.map {|a| a.compact}
+  end
+end
 
 class Object
   def to_cents
     (to_s.gsub(/[^-.0-9]/, '').to_f * 100).to_i
+  end
+end
+
+class Date
+  def last_sunday
+    d = self
+    d -= 1 until d.wday == 0
+    d
   end
 end
 
@@ -58,6 +73,7 @@ private
   end
 
   def get_form(body, name)
+    Hpricot.buffer_size = 262144
     doc = Hpricot.parse(body)
     form = (doc/"##{name}").first
     fields = Hash[*((form/"input").map {|e| [ e.attributes['name'], e.attributes['value'] ]}.flatten)]
@@ -94,64 +110,96 @@ def get_field(field)
   config[field]
 end
 
-def parse_register(body)
-  doc = Hpricot.parse(body)
-  (doc/"#grdHistory"/"tr").map do |e|
-    next nil unless (e['class'] == 'item' || e['class'] == 'alternatingItem')
+class CommerceBank
+  attr_reader :register
 
-    anchor = e.at("a")
-    values = (e/"td").map {|e1| e1.inner_html}
-    debit = values[3].to_cents
-    credit = values[4].to_cents
-    delta = credit - debit
-    total = values[5].to_cents
+  def initialize
+    client = WebClient.new
 
-    { :destination => anchor.inner_html.strip,
-      :url => anchor['href'],
-      :date => Date.parse(values[0]),
-      :delta => delta,
-      :debit => debit,
-      :credit => credit,
-      :total => total }
-  end.compact
+    client.get('/')
+
+    client.get('/CBI/login.aspx', 'MAINFORM')
+
+    client.fields['txtUserID'] = get_field('username')
+    response = client.post('/CBI/login.aspx', 'MAINFORM')
+
+    # If a question was asked, answer it then get the password page.
+    question = response.body.scan(/Your security question:&nbsp;&nbsp;(.*?)<\/td>/i).first.andand.first
+    if question
+      client.fields['txtChallengeAnswer'] = get_field(question)
+      client.fields['saveComputer'] = 'rdoBindDeviceNo'
+      response = client.post('/CBI/login.aspx', 'MAINFORM')
+    end
+
+    raise "could not reach the password page" unless client.fields['__EVENTTARGET'] == 'btnLogin'
+
+    client.fields['txtPassword'] = get_field('password')
+    response = client.post('/CBI/login.aspx')
+
+    response = client.get('/CBI/Accounts/CBI/Activity.aspx', 'MAINFORM')
+
+    client.fields['Anthem_UpdatePage'] = 'true'
+    client.fields['txtFilterFromDate:textBox'] = Time.parse('1/1/2000').strftime('%m/%d/%Y')
+    client.fields['txtFilterToDate:textBox'] = Time.now.strftime('%m/%d/%Y')
+    response = client.post('/CBI/Accounts/CBI/Activity.aspx?Anthem_CallBack=true')
+
+    raw_data = JSON.parse(response.body)
+    @register = parse_register(raw_data['controls']['pnlPosted'])
+  end
+
+  def summary
+    today, yesterday, this_week, last_week = [], [], [], []
+
+    register.each do |entry|
+      if    entry[:date] == Date.today                               then today << entry
+      elsif entry[:date] == (Date.today - 1)                         then yesterday << entry
+      elsif entry[:date] >= Date.today.last_sunday                   then this_week << entry
+      elsif entry[:date] >= (Date.today.last_sunday - 1).last_sunday then last_week << entry
+      end
+    end
+
+    [ summarize('Today', today), 
+      summarize('Yesterday', yesterday),
+      summarize('This Week', this_week),
+      summarize('Last Week', last_week) ].compact.join("\n")
+  end
+
+private
+
+  def summarize(label, entries)
+    return nil if entries.length == 0
+
+    text = label.to_s + ":\n"
+
+    text + entries.map do |e| 
+      delta = "%s%0.2f" % [ (e[:delta] >= 0 ? '+' : '-'), e[:delta].abs/100.0 ] 
+      "%-100s %10s %10.2f\n" % [ e[:destination], delta, e[:total]/100.0 ]
+    end.join
+  end
+
+  def parse_register(body)
+    Hpricot.buffer_size = 262144
+    doc = Hpricot.parse(body)
+    coder = HTMLEntities.new
+    (doc/"#grdHistory"/"tr").map do |e|
+      next nil unless (e['class'] == 'item' || e['class'] == 'alternatingItem')
+
+      anchor = e.at("a")
+      values = (e/"td").map {|e1| e1.inner_html}
+      debit = values[3].to_cents
+      credit = values[4].to_cents
+      delta = credit - debit
+      total = values[5].to_cents
+
+      { :destination => coder.decode(anchor.inner_html.strip),
+        :url => anchor['href'],
+        :date => Date.parse(values[0]),
+        :delta => delta,
+        :debit => debit,
+        :credit => credit,
+        :total => total }
+    end.compact
+  end
 end
 
-Hpricot.buffer_size = 262144
-
-client = WebClient.new
-
-client.get('/')
-
-client.get('/CBI/login.aspx', 'MAINFORM')
-
-client.fields['txtUserID'] = get_field('username')
-response = client.post('/CBI/login.aspx', 'MAINFORM')
-
-# If a question was asked, answer it then get the password page.
-question = response.body.scan(/Your security question:&nbsp;&nbsp;(.*?)<\/td>/i).first.andand.first
-if question
-  client.fields['txtChallengeAnswer'] = get_field(question)
-  client.fields['saveComputer'] = 'rdoBindDeviceNo'
-  response = client.post('/CBI/login.aspx', 'MAINFORM')
-end
-
-if client.fields['__EVENTTARGET'] == 'btnLogin'
-  client.fields['txtPassword'] = get_field('password')
-  response = client.post('/CBI/login.aspx')
-  response = client.get('/CBI/Accounts/CBI/Activity.aspx', 'MAINFORM')
-else
-  puts "Could not reach the password page."
-  pp client.fields
-  puts response.body
-  exit
-end
-
-client.fields['Anthem_UpdatePage'] = 'true'
-client.fields['txtFilterFromDate:textBox'] = Time.parse('1/1/2000').strftime('%m/%d/%Y')
-client.fields['txtFilterToDate:textBox'] = Time.now.strftime('%m/%d/%Y')
-response = client.post('/CBI/Accounts/CBI/Activity.aspx?Anthem_CallBack=true')
-
-raw_data = JSON.parse(response.body)
-register = parse_register(raw_data['controls']['pnlPosted'])
-pp register
-
+puts CommerceBank.new.summary
